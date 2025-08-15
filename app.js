9234e7ad37fa8a5d7a43c2cee86ecb309b0af4f6
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-08-15 23:15:31 +0545

    Refactor code formatting and improve readability in app.js

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ connectToDatabase()
 const Library = require("./model/bookModel")
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-
+app.use(express.urlencoded({ extended: true }))
 
 app.post("/create", async (req, res) => {
     const { bookName, bookDescription, bookPrice, authorName, publishedAt, publication } = req.body
-    if(!bookName || !bookDescription || !bookPrice || !authorName || !publishedAt || !publication){
+    if (!bookName || !bookDescription || !bookPrice || !authorName || !publishedAt || !publication) {
         return res.status(400).json({
             message: "Please provide all the required fields: bookName, bookDescription, bookPrice, authorName, publishedAt, and publication."
         })
@@ -32,10 +30,23 @@ app.post("/create", async (req, res) => {
 })
 
 
-app.get("/books", async (req,res)=>{
-    const books = await Library.find()
+app.get("/books", async (req, res) => {
+    const books = await Library.find() //Array
     return res.status(200).json({
         data: books
+    })
+})
+
+app.get("/book/:id", async (req, res) => {
+    const { id } = req.params
+    const book = await Library.findById(id) //Object
+    if (!book) {
+        return res.status(400).json({
+            message: "Book not found."
+        })
+    }
+    return res.status(200).json({
+        data: book
     })
 })
 
